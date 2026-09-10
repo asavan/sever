e32fdc2ef59593941cde4a1c5edb32616ca1eb8f
a.savanovich <asavanster@gmail.com>
2026-09-10 17:50:28 +0300

    try register service worker new way

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import settings from "./js/settings.js";
 
 
 if (__USE_SERVICE_WORKERS__) {
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("./sw.js", {scope: "./"});
+    if ('serviceWorker' in navigator) {
+        // Вычисляет абсолютный путь к sw.js относительно файла index.html
+        const swUrl = new URL('sw.js', import.meta.url || window.location.href).href;
+
+        navigator.serviceWorker.register(swUrl)
+            .then((reg) => console.log('Успешный scope:', reg.scope));
     }
 }
 
