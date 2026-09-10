9a2634d17b7d7e65654d62e4d1b78a426068729d
a.savanovich <asavanster@gmail.com>
2026-09-10 18:14:28 +0300

    revert to old sw registration

--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ import settings from "./js/settings.js";
 
 if (__USE_SERVICE_WORKERS__) {
     if ("serviceWorker" in navigator) {
-        // Вычисляет абсолютный путь к sw.js относительно файла index.html
-        const swUrl = new URL("sw.js", import.meta.url || window.location.href).href;
-
-        navigator.serviceWorker.register(swUrl)
-            .then((reg) => console.log("Успешный scope:", reg.scope));
+        navigator.serviceWorker.register("./sw.js", {scope: "./"});
     }
 }
 
